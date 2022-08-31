f27799af0a207e980b21c3c08b3ae9cf61a448d4
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-08-30 21:53:36 -0300

    add search by title and search by date function to search engine

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,32 @@
+import tech_news.database as db
+import datetime as dt
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    mongoData = db.search_news({"title": {"$regex": title, "$options": "i"}})
+    list = []
+    for item in mongoData:
+        list.append((item["title"], item["url"]))
+    return list
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    # date received "2021-04-04"
+    # ISO Date: 2021-04-04T00:00:00.000000
+
+    try:
+        mydate = dt.date.fromisoformat(date).strftime("%d/%m/%Y")
+        mongoData = db.search_news({"timestamp": mydate})
+
+        list = []
+        for item in mongoData:
+            list.append((item["title"], item["url"]))
+
+        return list
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
